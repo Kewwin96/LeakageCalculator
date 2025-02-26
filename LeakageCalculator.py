6b3459438dc62565ddfa43d902849fd3b398e9b4
Kewwin96 <kevin.sres96@hotmail.com>
2025-02-26 12:06:29 +0100

    Update LeakageCalculator.py

--- LeakageCalculator.py
+++ LeakageCalculator.py
@@ -55,6 +55,7 @@ def calculate(tab):
 <<<<<<< Updated upstream
         tab.result_output.delete("1.0", tk.END)  # Clear previous results
 =======
+        tab.result_output.delete("1.0", tk.END)
 >>>>>>> Stashed changes
         tab.result_output.insert(tk.END, "\n".join(results))
         tab.result_output.config(state=tk.DISABLED)
